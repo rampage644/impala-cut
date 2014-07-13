96fbe2a8f9b675296cde97d315bc9dffd1414aeb
Sergei Turukin <rampage644@gmail.com>
2014-07-13 15:00:27 +0300

    Test modified

--- be/src/runtime/plan-fragment-executor-test.cc
+++ be/src/runtime/plan-fragment-executor-test.cc
@@ -2,6 +2,7 @@
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/transport/TFileTransport.h>
 
+#include "runtime/data-stream-mgr.h"
 #include "codegen/llvm-codegen.h"
 #include "runtime/plan-fragment-executor.h"
 #include "runtime/row-batch.h"
@@ -13,52 +14,54 @@
 #include "gen-cpp/ImpalaInternalService_types.h"
 #include "gen-cpp/ImpalaInternalService.h"
 
-void load_request(impala::TExecPlanFragmentParams& req)
+#define REQUEST(file) \
+  impala::TExecPlanFragmentParams request; \
+  load_request(file, request);
+
+#define REQUEST_SELECT_1 REQUEST("query_select_1002.bin");
+#define REQUEST_SELECT_FROM_TBL REQUEST("query_select_from_tbl.bin");
+
+boost::scoped_ptr<impala::ExecEnv> env;
+
+void load_request(const char* filename, impala::TExecPlanFragmentParams& req)
 {
   using namespace apache::thrift::protocol;
   using namespace apache::thrift::transport;
 
-  boost::shared_ptr<TFileTransport> transport(new TFileTransport("TExecPlanFragmentParams.bin", true));
+  boost::shared_ptr<TFileTransport> transport(new TFileTransport(filename, true));
   boost::shared_ptr<TBinaryProtocol> proto(new TBinaryProtocol(transport));
   req.read(proto.get());
 }
 
-TEST(PlanFragmentExecutorTest, Basic)
+TEST(ExecutorTest, Basic)
 {
-    impala::ExecEnv* env = new impala::ExecEnv();
     impala::PlanFragmentExecutor* ptr;
-    ptr = new impala::PlanFragmentExecutor(env,
+    ptr = new impala::PlanFragmentExecutor(env.get(),
                                            NULL);
-    impala::TExecPlanFragmentParams params;
-    load_request(params);
-    ASSERT_EQ(ptr->Prepare(params).code(), impala::TStatusCode::OK);
+    REQUEST_SELECT_1
+    ASSERT_EQ(ptr->Prepare(request).code(), impala::TStatusCode::OK);
     delete ptr;
-    delete env;
 }
 
-TEST(PlanFragmentExecutorTest, JustOpen)
+TEST(ExecutorTest, JustOpen)
 {
   using namespace impala;
-  ExecEnv env;
-  PlanFragmentExecutor exec(&env, NULL);
+  PlanFragmentExecutor exec(env.get(), NULL);
 
-  TExecPlanFragmentParams params;
-  load_request(params);
+  REQUEST_SELECT_1
 
-  ASSERT_EQ(exec.Prepare(params).code(), TStatusCode::OK);
+  ASSERT_EQ(exec.Prepare(request).code(), TStatusCode::OK);
   ASSERT_EQ(exec.Open().code(), TStatusCode::OK);
 }
 
-TEST(PlanFragmentExecutorTest, GetResult)
+TEST(ExecutorTest, GetResult)
 {
   using namespace impala;
-  ExecEnv env;
-  PlanFragmentExecutor exec(&env, NULL);
+  PlanFragmentExecutor exec(env.get(), NULL);
 
-  TExecPlanFragmentParams params;
-  load_request(params);
+  REQUEST_SELECT_1
 
-  ASSERT_EQ(exec.Prepare(params).code(), TStatusCode::OK);
+  ASSERT_EQ(exec.Prepare(request).code(), TStatusCode::OK);
   ASSERT_EQ(exec.Open().code(), TStatusCode::OK);
 
   RowBatch* batch = NULL;
@@ -66,6 +69,23 @@ TEST(PlanFragmentExecutorTest, GetResult)
 
   ASSERT_EQ(batch->num_rows(), 1) << "Result should have 1 row.";
   ASSERT_STREQ(PrintRow(batch->GetRow(0), batch->row_desc()).c_str(), "[(1002)]");
+}
+
+TEST(ExecutorTest, SelectFromTable)
+{
+  using namespace impala;
+  PlanFragmentExecutor exec(env.get(), NULL);
+
+  REQUEST_SELECT_FROM_TBL
+
+  ASSERT_NE(ExecEnv::GetInstance(), (ExecEnv*)NULL);
+  ASSERT_EQ(exec.Prepare(request).code(), TStatusCode::OK);
+  ASSERT_EQ(exec.Open().code(), TStatusCode::OK);
+
+  RowBatch* batch = NULL;
+  env->stream_mgr()-
+  ASSERT_EQ(exec.GetNext(&batch).code(), TStatusCode::OK);
+
 }
 
 
@@ -77,6 +97,7 @@ int main(int argc, char **argv) {
   impala::InitThreading();
   impala::TimestampParser::Init();
   impala::LlvmCodeGen::InitializeLlvm();
+  env.reset(new impala::ExecEnv());
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
