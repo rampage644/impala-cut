21aeac90c176cbc859be88dd15cac1c1980a932a
Sergei Turukin <rampage644@gmail.com>
2014-07-14 17:50:03 +0300

    Printer extended

--- printer.cc
+++ printer.cc
@@ -1,25 +1,45 @@
 #include <iostream>
+#include <string>
 #include <netinet/in.h>
 
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/protocol/TDebugProtocol.h>
 #include <thrift/transport/TFileTransport.h>
 #include "be/generated-sources/gen-cpp/ImpalaInternalService_types.h"
+#include "be/generated-sources/gen-cpp/Frontend_types.h"
 
-int main(int argc, char** argv) {
-  if (argc < 2)
-  {
-    std::cerr << "No filename found" << std::endl;
-    return 1;
-  }
+
+
+template <class T>
+void dumps(const char* filename) 
+{
   using namespace apache::thrift::transport;
   using namespace apache::thrift::protocol;
   using namespace apache::thrift;
-  impala::TExecPlanFragmentParams req;
+  T obj; 
 
-  boost::shared_ptr<TFileTransport> transport(new TFileTransport(argv[1]));
+  boost::shared_ptr<TFileTransport> transport(new TFileTransport(filename));
   boost::shared_ptr<TBinaryProtocol> proto(new TBinaryProtocol(transport));
-  req.read(proto.get());
-  std::cout << ThriftDebugString(req) << std::endl;
-  return 0;
+  obj.read(proto.get());
+  std::cout << ThriftDebugString(obj) << std::endl;
 }
+
+int main(int argc, char** argv) {
+  std::string classname = "TExecPlanFragmentParams";
+  if (argc < 3)
+  {
+    std::cerr << "Not enough argv" << std::endl;
+    return 1;
+  }
+
+
+  if (!strcmp(argv[2], "TExecPlanFragmentParams")) 
+    dumps<impala::TExecPlanFragmentParams>(argv[1]);
+  else if (!strcmp(argv[2], "TQueryExecRequest"))
+    dumps<impala::TQueryExecRequest>(argv[1]);
+  else if (!strcmp(argv[2], "TQueryOptions"))
+    dumps<impala::TQueryOptions>(argv[1]);
+
+  return 1;
+}
+
